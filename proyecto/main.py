9e76511fd737a54bed0e2c0684070892f2230879
Esteban Lucero <este.lucero2212@gmail.com>
2024-03-20 19:45:47 -0300

    Agrego icono de futbol y fix

--- proyecto/main.py
+++ proyecto/main.py
@@ -3,15 +3,22 @@ from kivy.uix.screenmanager import ScreenManager
 from kivy.core.text import LabelBase
 from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.app import App
+
 Window.size= (350,580)
 
 class Canchas(MDApp):
     def build(self):
+
+
         global screen_manager
         screen_manager=ScreenManager()
         screen_manager.add_widget(Builder.load_file("loginTouch.kv"))
         ##screen_manager.add_widget(Builder.load_file("chatBot.kv"))
+
         return screen_manager
+
+
 
 LabelBase.register(name="BMont",fn_regular="assets\\static\\Montserrat-Bold.ttf")
 Canchas().run()
